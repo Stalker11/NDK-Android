f16f2600b60578738e76492035a3227e62f642ef
Oleh Mushenko <Oleh.Mushenko@dish.com>
2020-05-12 17:43:09 +0300

    Called C++ from kotlin and kotlin from C++

---
+++ app/src/main/cpp/jni-call.h
@@ -0,0 +1,10 @@
+
+#include "../../../../../../Library/Android/sdk/ndk/20.0.5594570/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/jni.h"
+
+#ifndef ANDROID_NDK_QT_JNI_CALL_H
+#define ANDROID_NDK_QT_JNI_CALL_H
+extern "C" {
+#endif //ANDROID_NDK_QT_JNI_CALL_H
+JNIEXPORT void JNICALL
+Java_com_olegel_androidndkqt_MainActivity_setStr(JNIEnv *env, jobject type, jstring text);
+};
